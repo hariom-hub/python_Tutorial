f8540e4e7a82d5179ebf1ae0b308e5e230076a3a
Hariom Singh Thakur <hariomthakur638@gmail.com>
2024-09-26 20:22:33 +0530

    studied one hour

---
+++ chapter2/file1.py
@@ -0,0 +1,12 @@
+def main():
+    a = True
+    return a
+
+print(main())
+
+#type casting in python
+a = 99.3
+print(type(a))
+b = str(a)
+print(type(b))
+print(b)
---
+++ joke.py
@@ -0,0 +1,6 @@
+import pyjokes
+joke = pyjokes.get_joke()
+for x in range(4):
+    print(joke)
+    #this is a python code
+print("hello")
