483a96b30c9306d85babd24c3c7336894c290672
Hariom Singh Thakur <hariomthakur638@gmail.com>
2024-10-08 01:46:44 +0530

    files saved

---
+++ strings/stringfn.py
@@ -0,0 +1,10 @@
+#string functions
+name = "hariom singh thakur"
+print(len(name))
+print(name.endswith("kur"))
+print(name.startswith("hario"))
+print(name.count("a"))
+print(name.capitalize())
+print(name.replace("hariom","shreesha"))
+print(name.split())
+print(name.upper())
