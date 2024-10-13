e9d5fda950f4999d5919e1da2a3475a9f8e5e3c0
Hariom Singh Thakur <hariomthakur638@gmail.com>
2024-10-14 00:40:18 +0530

    list & tuples completed

---
+++ lists_tuples/practice_list_tuple/file1.py
@@ -0,0 +1,58 @@
+
+#QUESTION : 1
+# fruits = []
+# f1 = input(Enter fruit name : ")
+# fruits.append(f1)
+# f2 = input("Enter fruit name : ")
+# fruits.append(f2)
+# f3 = input("Enter fruit name : ")
+# fruits.append(f3)
+# f4 = input("Enter fruit name : ")
+# fruits.append(f4)
+# f5 = input("Enter fruit name : ")
+# fruits.append(f5)
+# f6 = input("Enter fruit name : ")
+# fruits.append(f6)
+# f7 = input("Enter fruit name : ")
+# fruits.append(f7)
+#
+# print(fruits)
+
+#printing fruits using loops
+
+# i=1
+# fruits2 = []
+# for x in range(7):
+#     fruit = input(f"Enter the {i}th fruit name : ")
+#     fruits2.append(fruit)
+#     i+=1
+#
+# print("Fruits Entered by the user")
+# print(fruits2)
+
+#QUESTION : 2
+
+# marks = []
+#taking marks as input
+# i = 1
+# for x in range(6):
+#     mark = input(f"Enter the marks of student {i} : ")
+#     i += 1
+#     marks.append(mark)
+#
+# print("original sequence of marks entered : ")
+# print(marks)
+# print("sorted sequence of marks entered : ")
+# marks.sort()
+# print(marks)
+
+#QUESTION 4
+
+nums = (11,22,33,44)
+sum_num = sum(nums)
+print(sum_num)
+
+#QUESTION 5
+a = (7,0,8,0,0,9)
+print(a.count(0))
+
