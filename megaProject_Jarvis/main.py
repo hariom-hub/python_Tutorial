bfa52857285b927b39648690f8e7ddab05a7b6f2
Hariom Singh Thakur <hariomthakur638@gmail.com>
2024-11-23 16:14:09 +0530

    files saved

---
+++ megaProject_Jarvis/main.py
@@ -0,0 +1,14 @@
+
+import speech_recognition as sr
+# import webbrowser as wb
+import pyttsx3
+
+recognizer = sr.Recognizer()
+engine = pyttsx3.init()
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+if __name__ == "__main__":
+    speak("hello how can i help you")
---
+++ megaProject_Jarvis/projectinfo.md
@@ -0,0 +1,8 @@
+I this project, I am going to create an Assistant in python.
+<br/>
+
+**Packages installed**<br/>
+**Command - pip install packagename**
+* speechrecognition
+* pyaudio
+* setuptools
